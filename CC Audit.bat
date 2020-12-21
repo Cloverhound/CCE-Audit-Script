f3d896477a921d5cfc6e74c0d90fdc9f24426feb
jerky33 <75854254+jerky33@users.noreply.github.com>
2020-12-21 11:06:52 -0500

    Create CC Audit.bat

---
+++ CC Audit.bat
@@ -0,0 +1,1 @@
+start C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noprofile -executionpolicy bypass -file "C:\Users\user1\Documents\Code\PowerShell_Scripts\CCE-Audit-Script\CCE-Audit.ps1"
