0b5c9b10b1305d1a4fec1d6243e62690d134772b
jerky33 <75854254+jerky33@users.noreply.github.com>
2021-01-13 13:55:07 -0500

    Update CC Audit.bat

--- CC Audit.bat
+++ CC Audit.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-start C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noprofile -executionpolicy bypass -file ".\CCE-Audit.ps1"
-=======
 start C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -file ".\CCE-Audit.ps1"
->>>>>>> 593f83ca2c1402b0eb48cb1094e43af5857a5a07
